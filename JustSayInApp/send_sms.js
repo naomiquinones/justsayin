97fa7e7f67344ebcae9cdf51e0be5eeb585f577d
naomiquinones <naomiquinones@gmail.com>
2019-11-05 00:05:24 -0800

    Modify send_sms dotenv config path

--- JustSayInApp/send_sms.js
+++ JustSayInApp/send_sms.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
+require('dotenv').config({ path: '../.env'});
 
 // Twilio SMS code
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = require("twilio")(accountSid, authToken);
-
 twilioClient.messages
   .create({
-    body: "I am testing my final project send message function.",
+    body: "Today is November 5, 2019.",
     from: "+13236153004",
-    to: "+13109486677"
+    to: "+13102547608"
   })
   .then(message => console.log(message.sid));
 // end Twilio SMS code
