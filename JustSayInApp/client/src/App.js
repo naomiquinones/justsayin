36b71405347cf9da641fb8d45903af36f93af642
naomiquinones <naomiquinones@gmail.com>
2019-11-07 17:22:55 -0800

    Maybe reformatted code?

--- JustSayInApp/client/src/App.js
+++ JustSayInApp/client/src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import JustSayInlogo from './JustSayIn-logo-wordmark.svg';
+import React, { Component } from "react";
+import "./App.css";
+import JustSayInlogo from "./JustSayIn-logo-wordmark.svg";
 import cookie from "react-cookies";
 
 import { googleTranslate } from "./utils/googleTranslate";
 // import SavedTexts from './components/SavedTexts';
-import FormContainer from './components/FormContainer';
-
+import FormContainer from "./components/FormContainer";
 
 class App extends Component {
   state = {
     languageCodes: [],
     language: cookie.load("language") ? cookie.load("language") : "en",
-    question: cookie.load("question") ? cookie.load("question") : "What language do you prefer to use?"
+    question: cookie.load("question")
+      ? cookie.load("question")
+      : "What language do you prefer to use?"
   };
 
   componentDidMount() {
     googleTranslate.getSupportedLanguages("en", function(err, languageCodes) {
       getLanguageCodes(languageCodes);
     });
-    
+
     const getLanguageCodes = languageCodes => {
       this.setState({ languageCodes });
     };
@@ -29,15 +30,20 @@ class App extends Component {
     const { languageCodes, language, question } = this.state;
 
     return (
-      <div className="App" style={this.divStyle}>
+      <div className="App">
         <header className="page-header">
           <h1>
-            <img src={JustSayInlogo} className="JustSayIn-logo" alt="Just Say In" title="Just Say In"/>
-          </h1>        
+            <img
+              src={JustSayInlogo}
+              className="JustSayIn-logo"
+              alt="Just Say In"
+              title="Just Say In"
+            />
+          </h1>
         </header>
         <main>
           {/* <SavedTexts /> */}
-          <p>{ question }</p>
+          <p>{question}</p>
           <FormContainer />
         </main>
         <footer className="page-footer">
