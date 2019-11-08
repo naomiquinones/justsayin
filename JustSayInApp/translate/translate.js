d8e70de7c759f40d66d44f31fa53a1176c056a03
naomiquinones <naomiquinones@gmail.com>
2019-11-07 17:20:13 -0800

    Change text

--- JustSayInApp/translate/translate.js
+++ JustSayInApp/translate/translate.js
@@ -2,11 +2,9 @@ const axios = require("axios");
 
 /* axios
   .get(
-    'https://translation.googleapis.com/language/translate/v2?target={APIKEY}&q="George went to the grocery store this morning, thinking he would buy a gallon of milk. Instead, he came out with a tub of ice cream, and two cans of anchovies."'
+    'https://translation.googleapis.com/language/translate/v2?target={APIKEY}&q="George went to the grocery store this morning, thinking he would buy a gallon of milk. Instead, he came out with a tub of ice cream, two cans of anchovies and a bag of kale."'
   )
   .then(res => {
     console.log(res.data.data.translations[0].translatedText);
   });
  */
-
- 
