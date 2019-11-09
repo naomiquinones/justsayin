8e8ce83034aee4cf2b76c3398902e369c5568016
naomiquinones <naomiquinones@gmail.com>
2019-11-09 00:38:19 -0800

    Delete dotenv

--- JustSayInApp/client/src/utils/googleTranslate.js
+++ JustSayInApp/client/src/utils/googleTranslate.js
@@ -1,9 +1,3 @@
-require("dotenv").config({ path: "./../../../.env" });
 const apiKey = process.env.JUST_SAY_IN_GOOGLE_TRANSLATE_API_KEY;
-
-console.log(
-  "In googleTranslate.js, process.env.GOOGLE_TRANSLATE_API_KEY is ",
-  process.env.JUST_SAY_IN_GOOGLE_TRANSLATE_API_KEY
-);
 
 export const googleTranslate = require("google-translate")(apiKey);
