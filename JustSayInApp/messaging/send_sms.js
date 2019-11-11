2bd1173feca2e11904708b581cb5784a9b3a3659
naomiquinones <naomiquinones@gmail.com>
2019-11-11 10:58:09 -0800

    Delete test file, clean up translate file, update send_sms to today's date

--- JustSayInApp/messaging/send_sms.js
+++ JustSayInApp/messaging/send_sms.js
@@ -6,7 +6,7 @@ const send = recipient => {
   const twilioPhone = process.env.TWILIO_PHONE_NUMBER;
   twilioClient.messages
     .create({
-      body: "Testing a new message from my app. Today is November 7, 2019.",
+      body: "Testing a new message from my app. Today is November 11, 2019.",
       from: twilioPhone,
       to: recipient
     })
--- JustSayInApp/translate/translate.js
+++ JustSayInApp/translate/translate.js
@@ -1,22 +1,20 @@
 require("dotenv").config();
-// const axios = require("axios");
 
-/* axios
-  .get(
-    'https://translation.googleapis.com/language/translate/v2?target={APIKEY}&q="George went to the grocery store this morning, thinking he would buy a gallon of milk. Instead, he came out with a tub of ice cream, two cans of anchovies and a bag of kale."'
-  )
-  .then(res => {
-    console.log(res.data.data.translations[0].translatedText);
-  });
- */
+// get googleTranslate with API key
 const googleTranslate = require("../utils/googleTranslate");
 
-const textToTranslate =
+let textToTranslate =
   "George went to the grocery store this morning, thinking he would buy a gallon of milk. Instead, he came out with a tub of ice cream, two cans of anchovies and a bag of kale.";
 
-googleTranslate.translate(textToTranslate, "es", function(err, translation) {
+let sourceLanguage = "en";
+let targetLanguage = "es";
+
+googleTranslate.translate(textToTranslate, targetLanguage, function(
+  err,
+  translation
+) {
   console.log(translation.translatedText);
 });
-googleTranslate.getSupportedLanguages("en", (err, codes) => {
+googleTranslate.getSupportedLanguages(sourceLanguage, (err, codes) => {
   console.log(codes);
 });
