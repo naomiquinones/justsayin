2ab96001a1037c817942f943a1c289d0410bf7b0
naomiquinones <naomiquinones@gmail.com>
2019-11-11 00:52:49 -0800

    Change name of variable

--- JustSayInApp/translate/translate.js
+++ JustSayInApp/translate/translate.js
@@ -9,15 +9,16 @@ require("dotenv").config();
     console.log(res.data.data.translations[0].translatedText);
   });
  */
-const apiKey = process.env.JUST_SAY_IN_GOOGLE_TRANSLATE_API_KEY;
+const apiKey = process.env.JUST_SAY_IN_APP_GOOGLE_TRANSLATE_API_KEY;
+// console.log(apiKey);
 const googleTranslate = require("google-translate")(apiKey);
 
-// const textToTranslate =
-//   "George went to the grocery store this morning, thinking he would buy a gallon of milk. Instead, he came out with a tub of ice cream, two cans of anchovies and a bag of kale.";
+const textToTranslate =
+  "George went to the grocery store this morning, thinking he would buy a gallon of milk. Instead, he came out with a tub of ice cream, two cans of anchovies and a bag of kale.";
 
-// googleTranslate.translate(textToTranslate, "es", function(err, translation) {
-//   console.log(translation.translatedText);
-// });
+googleTranslate.translate(textToTranslate, "es", function(err, translation) {
+  console.log(translation.translatedText);
+});
 googleTranslate.getSupportedLanguages("en", (err, codes) => {
   console.log(codes);
 });
