80390648f45bc47df9b61ca8becdc2497132251b
naomiquinones <naomiquinones@gmail.com>
2019-11-11 16:24:25 -0800

    Change const name in translate file

--- JustSayInApp/translate/translate.js
+++ JustSayInApp/translate/translate.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 
-// get googleTranslate with API key
-const translator = require("../utils/googleTranslate");
+// const
+const run = require("../utils/googleTranslate");
 
 let textToTranslate = "Baby Miki was born on Saturday, November 9, 2019.";
 
 let sourceLanguage = "en";
 let targetLanguage = "es";
 
-translator.translate(textToTranslate, targetLanguage, (err, translation) => {
+run.translate(textToTranslate, targetLanguage, (err, translation) => {
   console.log(translation.translatedText);
 });
-translator.getSupportedLanguages(sourceLanguage, (err, codes) => {
+run.getSupportedLanguages(sourceLanguage, (err, codes) => {
   console.log(codes);
 });
