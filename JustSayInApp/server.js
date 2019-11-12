94fa6d8437fcb0a3e709ee2298c17fd39b7306f9
naomiquinones <naomiquinones@gmail.com>
2019-11-12 14:36:43 -0800

    Change variable name, comment out settings

--- JustSayInApp/server.js
+++ JustSayInApp/server.js
@@ -16,7 +16,7 @@ const viewSMS = require("./messaging/view_sms");
 const sendSMS = require("./messaging/send_sms");
 
 // bring in translation file
-const translator = require("./translate/translate");
+const translate = require("./translate/translate");
 
 // test endpoint
 app.get("/api/test", (req, res) => {
@@ -30,35 +30,37 @@ app.get("/api/test", (req, res) => {
 
 // translation endpoint
 app.post("/api/translate", (req, res) => {
-  const toTranslate = req.body.text;
-  const sourceLang = req.body.source;
-  const targetLang = req.body.target;
+  console.log(req.body.text, req.body.source, req.body.target);
+  // const textToTranslate = req.body.text;
+  // const sourceLang = req.body.source;
+  // const targetLang = req.body.target;
 
   // for (let lang of targetLangs) {
-  translator(toTranslate, sourceLang, targetLang);
+  translate("Very small dots are hard to see", "en", "es");
+  // translate(textToTranslate, sourceLang, targetLang);
   // }
   res.sendStatus(200);
 });
 
 app.post("/api/message", (req, res) => {
   console.log(req.body);
-  // const { recipient } = req.body;
+  const { recipient } = req.body;
 
   // Send a message
-  // let recipients = req.recipient || [process.env.TEST_RECIPIENT1];
+  let recipients = req.recipient || [process.env.TEST_RECIPIENT1];
 
-  // let msg = "Special message from the Just Say In app";
-  // for (let recipient of recipients) {
-  //   sendSMS.send(recipient, msg);
-  // }
-  // console.log(recipient);
+  let msg = "Special message from the Just Say In app";
+  for (let recipient of recipients) {
+    sendSMS.send(recipient, msg);
+  }
+  console.log(recipient);
   res.sendStatus(200);
 });
 
 // Below post for twilio incoming
 app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
-  twiml.message("Thank you for helping test my app");
+  twiml.message("Thank you for helping test this app");
 
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
