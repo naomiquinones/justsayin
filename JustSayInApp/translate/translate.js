69e19c04a1bffa8a0536c7eec1e5d52e20b26167
naomiquinones <naomiquinones@gmail.com>
2019-11-11 17:15:20 -0800

    Change translate file into a module

--- JustSayInApp/translate/translate.js
+++ JustSayInApp/translate/translate.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 
-// const
-const run = require("../utils/googleTranslate");
+const translate = (textToTranslate, sourceLanguage, targetLanguage) => {
+  const run = require("../utils/googleTranslate");
 
-let textToTranslate = "Baby Miki was born on Saturday, November 9, 2019.";
+  let text =
+    textToTranslate || "Baby Miki was born on Saturday, November 9, 2019.";
 
-let sourceLanguage = "en";
-let targetLanguage = "es";
+  let source = sourceLanguage || "en";
+  let target = targetLanguage || "es";
 
-run.translate(textToTranslate, targetLanguage, (err, translation) => {
-  console.log(translation.translatedText);
-});
-run.getSupportedLanguages(sourceLanguage, (err, codes) => {
-  console.log(codes);
-});
+  run.translate(textToTranslate, targetLanguage, (err, translation) => {
+    console.log(translation.translatedText);
+  });
+  run.getSupportedLanguages(sourceLanguage, (err, codes) => {
+    console.log(codes);
+  });
+};
+module.exports = translate;
