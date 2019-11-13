1b8a302f1a4331602a82e67c551fbd952b099b7f
naomiquinones <naomiquinones@gmail.com>
2019-11-12 16:28:57 -0800

    Move module.exports to function itself, not below

--- JustSayInApp/translate/translate.js
+++ JustSayInApp/translate/translate.js
@@ -1,21 +1,22 @@
 require("dotenv").config();
-const translate = (
-  textToTranslate = "Hola, son las 3:42 de la tarde",
+module.exports = (
+  textToTranslate = "Hola, gracias por usar el app",
   sourceLanguage = "es",
   targetLanguage = "ja"
 ) => {
   const translator = require("../utils/googleTranslate");
 
-  translator.translate(
-    textToTranslate,
-    sourceLanguage,
-    targetLanguage,
-    (err, translation) => {
-      console.log(translation.translatedText);
-    }
-  );
+  const run = (textToTranslate, sourceLanguage, targetLanguage) => {
+    translator.translate(
+      textToTranslate,
+      sourceLanguage,
+      targetLanguage,
+      (err, translation) => {
+        console.log(translation.translatedText);
+      }
+    );
+  };
   // translator.getSupportedLanguages(sourceLanguage, (err, codes) => {
   //   console.log(codes);
   // });
 };
-module.exports = translate;
