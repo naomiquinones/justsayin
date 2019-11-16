4dadc7215b1c807aed1705ab71fc2547e542d397
naomiquinones <naomiquinones@gmail.com>
2019-11-16 09:20:00 -0800

    Uncomment lines in FormContainer

--- JustSayInApp/client/src/components/FormContainer.js
+++ JustSayInApp/client/src/components/FormContainer.js
@@ -28,14 +28,14 @@ class FormContainer extends Component {
   }
 
   render() {
-    console.log(this.props);
-    // const { languageCodes } = this.props.languageCodes;
+    console.log("in FormContainer", this.props);
+    const { languageCodes } = this.props.languageCodes;
     return (
       <form className="form-container" onSubmit={this.handleFormSubmit}>
         <TextArea placeholder={"Enter text to translate"} name={"sourceText"} />
         <TargetLanguageSelector
           name={"language-list"}
-          /* languages={languageCodes} */
+          languages={languageCodes}
         />
         <Input type={"submit"} value={"Translate"} />
       </form>
