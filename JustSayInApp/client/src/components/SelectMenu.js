68f06cc9c1e361394d59dc2ca62e9eed12e826dd
naomiquinones <naomiquinones@gmail.com>
2019-11-18 13:21:43 -0800

    Change select menu back to functional

--- JustSayInApp/client/src/components/SelectMenu.js
+++ JustSayInApp/client/src/components/SelectMenu.js
@@ -1,55 +1,60 @@
-import React , { Component } from "react";
+import React /* , { Component } */ from "react";
 
-class SelectMenu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedOption: null,
-    };
-  }
-  handleChange = selectedOption => {
-    this.setState({ selectedOption });
-    console.log(`Option selected: ${selectedOption}`);
-  }
-  render() {
-    const { selectedOption } = this.state;
-    return (
-      <div className="form-group">
-        <label htmlFor={this.props.name} className="form-label">{this.props.title}</label>
-        <select className="form-input language-list select-container"
-          id={this.props.name}
-          name={this.props.name}
-          value={selectedOption}
-          onChange={this.props.handleChange}
-          placeholder={this.props.placeholder}
-          options={options}
-        >
-        </select>
-      </div>
-    )
-  }
-}
+// class SelectMenu extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       selectedOption: null
+//     };
+//   }
+//   handleChange = selectedOption => {
+//     this.setState({ selectedOption });
+//     console.log(`Option selected: ${selectedOption}`);
+//   };
+//   render() {
+//     const optionsList = props.languages.map(language => {
+//       return language[0];
+//     });
+//     const { selectedOption } = this.state;
+//     return (
+//       <div className="form-group">
+//         <label htmlFor={this.props.name} className="form-label">
+//           {this.props.title}
+//         </label>
+//         <select
+//           className="form-input language-list select-container"
+//           id={this.props.name}
+//           name={this.props.name}
+//           value={selectedOption}
+//           onChange={this.props.handleChange}
+//           placeholder={this.props.placeholder}
+//           options={options}
+//         ></select>
+//       </div>
+//     );
+//   }
+// }
 
-// const SelectMenu = props => {
-//   // const {languages} = props.languages;
-//   const languageOptions = props.languages.map(language => {
-//   let optionsList += <option>{language[1]}</option>;
-//   });
-//   return (
-//     <div className="form-group select-group">
-//       <label htmlFor={props.name} className="form-label">
-//         {props.title}
-//       </label>
-//       <select
-//         className="form-input language-list select-container"
-//         id={props.name}
-//         name={props.name}
-//         onChange={props.handleChange}
-//         placeholder={props.placeholder}
-//       >
-        
-//       </select>
-//     </div>
-//   );
-// };
+const SelectMenu = props => {
+  const { languages } = props;
+  const languageOptions = languages.map(language => {
+    return <option>{language[1]}</option>;
+  });
+  return (
+    <div className="form-group select-group">
+      <label htmlFor={props.name} className="form-label">
+        {props.title}
+      </label>
+      <select
+        className="form-input language-list select-container"
+        id={props.name}
+        name={props.name}
+        onChange={props.handleChange}
+        placeholder={props.placeholder}
+      >
+        {languageOptions}
+      </select>
+    </div>
+  );
+};
 export default SelectMenu;
