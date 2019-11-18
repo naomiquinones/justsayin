63cc116e62b11cfacdc8c5357e979f72a4573d6b
naomiquinones <naomiquinones@gmail.com>
2019-11-18 13:22:04 -0800

    Change FormContainer to functional

--- JustSayInApp/client/src/components/FormContainer.js
+++ JustSayInApp/client/src/components/FormContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React /* , { Component } */ from "react";
 /* Import Components */
 // import CheckBox from '../components/CheckBox';
 import Input from "../components/Input";
@@ -6,44 +6,61 @@ import TextArea from "../components/TextArea";
 
 import TargetLanguageSelector from "../components/TargetLanguageSelector";
 import Loading from "../components/Loading";
-class FormContainer extends Component {
-  constructor(props) {
-    super(props);
+// class FormContainer extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    this.handleClearForm = this.handleClearForm.bind(this);
-  }
+//     this.state = {};
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+//     this.handleClearForm = this.handleClearForm.bind(this);
+//   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+//   handleChange(event) {
+//     this.setState({ value: event.target.value });
+//   }
 
-  handleFormSubmit() {
-    console.log("Submitting form");
-  }
-  handleClearForm() {
-    console.log("Clearing form");
-  }
+//   handleFormSubmit() {
+//     console.log("Submitting form");
+//   }
+//   handleClearForm() {
+//     console.log("Clearing form");
+//   }
 
-  render() {
-    const { languageCodes, isLoading } = this.props;
-    return (
-      <form className="form-container" onSubmit={this.handleFormSubmit}>
-        <TextArea placeholder={"Enter text to translate"} name={"sourceText"} />
-        {isLoading ? (
-          <Loading message={"Getting target languages"} />
-        ) : (
-          <TargetLanguageSelector
-            name={"language-list"}
-            languages={languageCodes}
-          />
-        )}
-        <Input type={"submit"} value={"Translate"} />
-      </form>
-    );
-  }
-}
+//   render() {
+//     const { languageCodes, isLoading } = this.props;
+//     return (
+//       <form className="form-container" onSubmit={this.handleFormSubmit}>
+//         <TextArea placeholder={"Enter text to translate"} name={"sourceText"} />
+//         {isLoading ? (
+//           <Loading message={"Getting target languages"} />
+//         ) : (
+//           <TargetLanguageSelector
+//             name={"language-list"}
+//             languages={languageCodes}
+//           />
+//         )}
+//         <Input type={"submit"} value={"Translate"} />
+//       </form>
+//     );
+//   }
+// }
 
+const FormContainer = props => {
+  const { languageCodes, isLoading } = props;
+  return (
+    <form className="form-container" onSubmit={props.handleSubmit}>
+      <TextArea placeholder={"Enter text to translate"} name={"sourceText"} />
+      {isLoading ? (
+        <Loading message={"Getting target languages"} />
+      ) : (
+        <TargetLanguageSelector
+          name={"language-list"}
+          languages={languageCodes}
+        />
+      )}
+      <Input type={"submit"} value={"Translate"} />
+    </form>
+  );
+};
 export default FormContainer;
