2bd53905c2814ddbc2bd8a77eca3b6de8dcd8fd8
naomiquinones <naomiquinones@gmail.com>
2019-11-19 15:02:16 -0800

    Remove commented test code

--- JustSayInApp/client/src/components/FormContainer.js
+++ JustSayInApp/client/src/components/FormContainer.js
@@ -6,45 +6,6 @@ import TextArea from "../components/TextArea";
 
 import TargetLanguageSelector from "../components/TargetLanguageSelector";
 import Loading from "../components/Loading";
-// class FormContainer extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {};
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleFormSubmit = this.handleFormSubmit.bind(this);
-//     this.handleClearForm = this.handleClearForm.bind(this);
-//   }
-
-//   handleChange(event) {
-//     this.setState({ value: event.target.value });
-//   }
-
-//   handleFormSubmit() {
-//     console.log("Submitting form");
-//   }
-//   handleClearForm() {
-//     console.log("Clearing form");
-//   }
-
-//   render() {
-//     const { languageCodes, isLoading } = this.props;
-//     return (
-//       <form className="form-container" onSubmit={this.handleFormSubmit}>
-//         <TextArea placeholder={"Enter text to translate"} name={"sourceText"} />
-//         {isLoading ? (
-//           <Loading message={"Getting target languages"} />
-//         ) : (
-//           <TargetLanguageSelector
-//             name={"language-list"}
-//             languages={languageCodes}
-//           />
-//         )}
-//         <Input type={"submit"} value={"Translate"} />
-//       </form>
-//     );
-//   }
-// }
 
 const FormContainer = props => {
   const {
--- JustSayInApp/client/src/components/TextArea.js
+++ JustSayInApp/client/src/components/TextArea.js
@@ -1,42 +1,4 @@
-import React /* , { Component } */ from "react";
-
-// class TextArea extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       textToTranslate: '',
-//       value: 'Text to translate'
-//     }
-//   }
-
-//   this.handleChange = this.handleChange.bind(this);
-//   this.handleSubmit = this.handleSubmit.bind(this);
-
-//   handleChange(event) {
-//     this.setState({value: event.target.value});
-//   }
-
-//   handleSubmit(event) {
-//     console.log('Text to translate: ' + this.state.value);
-//     event.preventDefault();
-//   }
-//   componentDidMount() {
-
-//   }
-
-//   render() {
-//     return (
-//       <div className="form-group textarea-container">
-//         <label htmlFor={props.name} className="form-label">{props.title}</label>
-//         <textarea className="form-input"
-//             id={props.name}
-//             name={props.name}
-//             value={this.state.value}
-//             onChange={this.handleChange}
-//         ></textarea>
-//     );
-//   }
-// }
+import React from "react";
 
 const TextArea = props => {
   const { name, title, placeholder, textToTranslate, handleChange } = props;
