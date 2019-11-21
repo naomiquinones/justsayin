ef1682a0f970641f1b0d5230ce1051eead4d0469
naomiquinones <naomiquinones@gmail.com>
2019-11-21 00:59:24 -0800

    Remove fontawesome

--- JustSayInApp/client/src/components/Loading.js
+++ JustSayInApp/client/src/components/Loading.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { fa-globe-americas } from '@fortawesome/free-solid-svg-icons'
 
 function Loading(props) {
   return <p className="loading-message">Loading...{props.message}</p>;
