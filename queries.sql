6f4b2847e36ce5ef5195ca71061ac18f5fe83040
naomiquinones <naomiquinones@gmail.com>
2019-11-26 09:50:20 -0800

    Move queries to new file, add db info

---
+++ queries.sql
@@ -0,0 +1,15 @@
+SELECT users.*, 
+FROM users 
+INNER JOIN (
+  SELECT b.contact_id
+  FROM users AS a
+  INNER JOIN user_contacts AS b on a.id = b.owner_id
+  WHERE a.id = 1--users id here
+) AS subquery ON users.id = subquery.contact_id
+;
+
+SELECT id, first_name, phone, target_lang_code 
+FROM users 
+WHERE id INTEGER
+ (SELECT contact_id FROM user_contacts WHERE owner_id=1)
+ ;
