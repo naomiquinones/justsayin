503d34a4d578ac6e109d99f45a1090071d56cce9
naomiquinones <naomiquinones@gmail.com>
2019-11-27 14:09:12 -0800

    Rearrange code blocks

--- JustSayInApp/client/src/App.js
+++ JustSayInApp/client/src/App.js
@@ -8,32 +8,16 @@ import Translation from "./components/Translation";
 
 import Messages from "./components/Messages";
 const App = () => {
-  // const sendMessages = recipients => {
-  //   // get list of unique recipients' target languages
-
-  //   // get the necessary translations
-
-  //   // match translations with recipients
-
-  //   // send all recipients with their translations to endpoint
-  //   for (let recipient of recipients) {
-  //     let number = recipient.name;
-  //     let msg = recipient.msg;
-  //     axios.post("/messages", number, msg);
-  //   }
-  // };
-
   return (
     <div className="App">
       <header className="page-header">
-        <h1>
-          <img
-            src={JustSayInlogo}
-            className="JustSayIn-logo"
-            alt="Just Say In"
-            title="Just Say In"
-          />
-        </h1>
+        <h1>Just Say In</h1>
+        <img
+          src={JustSayInlogo}
+          className="JustSayIn-logo"
+          alt="Just Say In"
+          title="Just Say In"
+        />
         <nav className="page-nav">
           <Link to="/">Home</Link>
           <Link to="/messages">Messages</Link>
