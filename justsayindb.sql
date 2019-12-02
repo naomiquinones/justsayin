054f9f158cb09119c233e8ede3272b85323deead
naomiquinones <naomiquinones@gmail.com>
2019-12-02 05:00:14 -0800

    Move insert code to seed-langs.sql

--- justsayindb.sql
+++ justsayindb.sql
@@ -41,22 +41,3 @@ CREATE TABLE messages (
   created_at TIMESTAMP
 );
 
-INSERT INTO languages (
-  iso_code,
-  language_name
-)
-VALUES
-('en','English'),
-('th','Thai'),
-('id','Indonesian'),
-('es','Spanish'),
-('zh','Chinese Simplified'),
-('ar','Arabic'),
-('fr','French'),
-('nl','Dutch'),
-('pt','Portuguese'),
-('ru','Russian'),
-('ja','Japanese')
-('ko','Korean'),
-('de','German')
-;
---
+++ seed-langs.sql
@@ -0,0 +1,19 @@
+INSERT INTO languages (
+  iso_code,
+  language_name
+)
+VALUES
+('en','English'),
+('th','Thai'),
+('id','Indonesian'),
+('es','Spanish'),
+('zh','Chinese Simplified'),
+('ar','Arabic'),
+('fr','French'),
+('nl','Dutch'),
+('pt','Portuguese'),
+('ru','Russian'),
+('ja','Japanese'),
+('ko','Korean'),
+('de','German')
+;
